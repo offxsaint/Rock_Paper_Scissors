49ffdad221ac5ed8e8c9a77f0a0dd71342c24520
offxsaint <211593962+offxsaint@users.noreply.github.com>
2026-04-11 02:16:19 +0200

    Added to UI.

--- script.js
+++ script.js
@@ -1,66 +1,96 @@
 let humanscore = 0;
 let computerscore = 0;
+let gameactive = true;
 
-function gethumanchoice() {
-    const options = ["rock", "paper", "scissors"];
-    const humanchoice = prompt("Choose your option rock, paper, or scissors").toLowerCase();
-    
-    if ("options.includes(humanchoice)") {
-    console.log(humanchoice);
-    return humanchoice;
-    }
-      else {
-        console.log("Invalid choice please choose rock, paper, scissors")
-        return gethumanchoice
-    }
-    
-};
+const result = document.querySelector("#results");
+const score = document.querySelector("#score");
+
+function updatescore () {
+    score.textContent = `Score: Player - ${humanscore}, Computer - ${computerscore}`;
+}
+
+function addresult (message) {
+    const resultmessage = document.createElement("p");
+    resultmessage.textContent = message;
+    result.appendChild(resultmessage);
+}
 
 function getcomputerchoice() {
     const options = ["rock", "paper", "scissors"];
     const optionchoice = options[Math.floor(Math.random() * 3)]
-    console.log(optionchoice);
+    addresult(optionchoice);
     return optionchoice;
 }
 
 function playround (humanchoice, computerchoice) {
+    if (!gameactive) {
+    addresult("Game is over!")
+    return;
+    }
+
+    addresult(humanchoice);
+
     if (humanchoice === computerchoice) {
-        console.log("It's a tie!");
+        addresult("It's a tie!");
     }
     else if (
         (humanchoice === "rock" && computerchoice === "scissors") ||
         (humanchoice === "paper" && computerchoice === "rock") ||
         (humanchoice === "scissors" && computerchoice === "paper")
     ) {
-        console.log("You win!");
+        addresult("You win!");
         humanscore++;
-        return "win!";
     }
     else {
-        console.log("You lose!");
+        addresult("You lose!");
         computerscore++;
-        return "lose!"
     }
+
+    updatescore();
+    addresult();
+
+    if (humanscore >= 5) {
+        addresult("You won the game");
+        addresult(`Score : Player - ${humanscore} , Computer - ${computerscore}`);
+        gameactive = false;
+    }
+
+else if (computerscore >= 5) {
+    addresult("You lost the game");
+    addresult(`Score : Player - ${humanscore} , Computer - ${computerscore}`);
+    gameactive = false;
+}
         
 };
 
-for (let round = 1;round <= 5; round++) {
-    console.log(`round ${round} of 5`);
 
-    const humanchoice = gethumanchoice();
-    const computerchoice = getcomputerchoice();
 
-    playround(humanchoice, computerchoice);
-    console.log(`Score You - ${humanscore}, Computer - ${computerscore}`);
-};
+const btnrock = document.querySelector("#btnrock");
+const btnpaper = document.querySelector("#btnpaper");
+const btnscissors = document.querySelector("#btnscissors");
 
-console.log("Game over");
-if (humanscore > computerscore) {
-    console.log(`Player wins :${humanscore} - ${computerscore}`);
-}
-else if (humanscore < computerscore) {
-    console.log(`Computer wins :${humanscore} - ${computerscore}`);
-}
-else {
-    console.log(`Player wins :${humanscore} - ${computerscore}`);
-};
+btnrock.addEventListener("click", ()=> {
+    if (gameactive) {
+        addresult("New Round");
+        playround("rock", getcomputerchoice())
+    }
+});
+
+btnpaper.addEventListener("click", ()=> {
+    if (gameactive) {
+        addresult("New Round");
+        playround("paper", getcomputerchoice())
+    }
+});
+
+btnscissors.addEventListener("click", ()=> {
+    if (gameactive) {
+        addresult("New Round");
+        playround("scissors", getcomputerchoice())
+    }
+});
+
+console.log(`Score: Player - ${humanscore}, Computer - ${computerscore}`);
+
+updatescore();
+addresult("Game Result");
